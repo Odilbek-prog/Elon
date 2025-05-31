58e3b9cd9a3302ec33723f70b68a2d639b7699b3
Odilbek-prog <odilbeknormuhamedov7@gmail.com>
2025-05-31 18:35:21 +0500

    file-sructure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-5 border border-black">Hello</div>
+    </>
+  );
+}
+
+export default App;
